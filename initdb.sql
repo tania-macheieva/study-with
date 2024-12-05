4e8fe05a9718a8e568691a4b0c910d54e6f3a25b
tania-macheieva bc2024-4 <macheieva.tetiana@gmail.com>
2024-12-05 14:17:55 +0200

    Created tables(categories, all_courses, saved_courses)

--- initdb.sql
+++ initdb.sql
@@ -8,7 +8,36 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
 INSERT INTO users (name, email, user_password, phone_number, role) VALUES
   ('John Doe', 'john@example.com', 'test1234', '+380685671890', 'teacher'),
   ('Jane Smith', 'jane@example.com', 'test5678', '+380976544121', 'student');
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,        
+    name VARCHAR(50) UNIQUE NOT NULL 
+);
+
+CREATE TABLE all_courses (
+    id SERIAL PRIMARY KEY,              
+    name VARCHAR(255) NOT NULL,         
+    author_id INT NOT NULL,             
+    price NUMERIC(10, 2),               
+    description TEXT,                   
+    category_id INT NOT NULL, 
+    image_url VARCHAR(1024),           
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_category
+        FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL
+);
+
+CREATE TABLE saved_courses (
+    id SERIAL PRIMARY KEY,                 
+    user_id INT NOT NULL,                 
+    course_id INT NOT NULL, 
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    CONSTRAINT fk_course
+        FOREIGN KEY (course_id) REFERENCES all_courses (id) ON DELETE CASCADE,
+    UNIQUE (user_id, course_id)           
+);
