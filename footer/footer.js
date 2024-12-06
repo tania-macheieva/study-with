e82dd7d57a518fce6464fffd5ad9ed58d21ee8ee
tania-macheieva bc2024-4 <macheieva.tetiana@gmail.com>
2024-12-06 19:02:23 +0200

    .

--- footer/footer.js
+++ footer/footer.js
@@ -1,16 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // Створюємо контейнер для футера
-  const footerContainer = document.createElement("div");
-  document.body.appendChild(footerContainer);
-
-  // Завантажуємо футер із файлу footer.html
   fetch("footer.html")
-      .then(response => {
-          if (!response.ok) throw new Error("Cannot load footer!");
-          return response.text();
-      })
-      .then(html => {
-          footerContainer.innerHTML = html;
-      })
-      .catch(error => console.error("Error loading footer:", error));
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("afterbegin", data);
+    });
 });
