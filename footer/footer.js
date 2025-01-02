ecefe07d27762d5181f0f4c1d1070476f88bb3e4
RomanMirka <romamirkastudy@gmail.com>
2025-01-02 14:43:44 +0200

    Ua Lang | Footer

--- footer/footer.js
+++ footer/footer.js
@@ -1,7 +1,57 @@
 document.addEventListener("DOMContentLoaded", () => {
+  // Отримуємо поточну мову з локального сховища або використовуємо 'en' за замовчуванням
+  const currentLang = localStorage.getItem('language') || 'en';
+  document.documentElement.lang = currentLang;
+
   fetch("/footer/footer.html")
-      .then((response) => response.text())
-      .then((data) => {
-          document.body.insertAdjacentHTML("beforeend", data);
-      });
+    .then((response) => response.text())
+    .then((data) => {
+      document.body.insertAdjacentHTML("beforeend", data);
+
+      // Перекладаємо футер відповідно до поточної мови
+      translateFooter(currentLang);
+    })
+    .catch((error) => {
+      console.error('Error loading footer:', error);
+    });
 });
+
+// Функція для перекладу футера
+const translateFooter = (lang) => {
+  const langData = footerTranslations[lang];
+
+  if (langData) {
+    document.querySelectorAll("[data-lang]").forEach((element) => {
+      const langKey = element.getAttribute("data-lang");
+      if (langData[langKey]) {
+        if (element.tagName === "INPUT") {
+          element.setAttribute("placeholder", langData[langKey]);
+        } else {
+          element.textContent = langData[langKey];
+        }
+      }
+    });
+  } else {
+    console.error(`No translations found for footer language: ${lang}`);
+  }
+};
+
+// Дані перекладу для футера
+const footerTranslations = {
+  en: {
+    footerReserved: "© 2024 All Rights Reserved StudyWith",
+    footerAbout: "About",
+    footerContact: "Contact",
+    footerFAQ: "FAQ",
+    footerTnC: "Terms & conditions",
+    footerPrivacy: "Privacy policy",
+  },
+  ua: {
+    footerReserved: "© 2024 Всі права захищені StudyWith",
+    footerAbout: "Про нас",
+    footerContact: "Контакти",
+    footerFAQ: "Поширені питання",
+    footerTnC: "Умови та положення",
+    footerPrivacy: "Політика конфіденційності",
+  },
+};
