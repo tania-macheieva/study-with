6030706d0525444c80f8e6baf63ddfd4166735d7
yuri <yurii.lytvyn@lnu.edu.ua>
2025-01-06 02:25:03 +0200

    2

--- initdb.sql
+++ initdb.sql
@@ -1,3 +1,4 @@
+-- Створення таблиці користувачів
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   name VARCHAR(255) NOT NULL,
@@ -8,41 +9,48 @@ CREATE TABLE users (
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   is_verified BOOLEAN DEFAULT FALSE
 );
+
+-- Створення таблиці викладачів
 CREATE TABLE teachers (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- Зв'язок із таблицею users
-    dob DATE,                          -- Дата народження
-    gender VARCHAR(50),                -- Стать
-    country VARCHAR(100),              -- Країна
-    city VARCHAR(100),                 -- Місто
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    dob DATE,
+    gender VARCHAR(50),
+    country VARCHAR(100),
+    city VARCHAR(100),
     phone_number VARCHAR(15),
-    zip_code VARCHAR(20),              -- Поштовий індекс
-    specialty VARCHAR(255),            -- Спеціалізація
-    professional_experience DATE,      -- Дата початку професійної діяльності
-    about TEXT,                        -- Коротка інформація
-    certificates BYTEA,                -- Сертифікати у форматі BLOB
+    zip_code VARCHAR(20),
+    specialty VARCHAR(255),
+    professional_experience DATE,
+    about TEXT,
+    certificates BYTEA,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Створення таблиці студентів
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE, -- Зв'язок із таблицею users
+    user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     date_of_birth DATE,
     phone_number VARCHAR(15),
     additional_info TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-
-INSERT INTO users (name, email, user_password, phone_number, role) VALUES
-    ('John Doe', 'john@example.com', 'test1234', '+380685671890', 'teacher'),
-    ('Jane Smith', 'jane@example.com', 'test5678', '+380976544121', 'student');
-
+-- Створення таблиці категорій
 CREATE TABLE categories (
-    id SERIAL PRIMARY KEY,        
-    name VARCHAR(50) UNIQUE NOT NULL 
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
 
+-- Створення таблиці рівнів освіти
+CREATE TABLE education_levels (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) UNIQUE NOT NULL,
+    description TEXT
+);
+
+-- Створення таблиці курсів
 CREATE TABLE all_courses (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
@@ -50,68 +58,68 @@ CREATE TABLE all_courses (
     price NUMERIC(10, 2),
     description TEXT NOT NULL,
     category_id INT NOT NULL,
-    image_url VARCHAR(1024), -- прев'ю курсу
-    education_level_id INT,  -- нове поле!
-    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories (id) ON DELETE SET NULL,
-    CONSTRAINT fk_education_level FOREIGN KEY (education_level_id) REFERENCES education_levels (id) ON DELETE SET NULL
+    image_url VARCHAR(1024),
+    education_level_id INT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_category FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE SET NULL,
+    CONSTRAINT fk_education_level FOREIGN KEY (education_level_id) REFERENCES education_levels(id) ON DELETE SET NULL
 );
 
--- -- !!!! якщо немає education_level_id у вже створеній таблиці all_courses
----------------------------------------------------------
--- ALTER TABLE all_courses
--- ADD COLUMN education_level_id INT,
--- ADD CONSTRAINT fk_education_level FOREIGN KEY (education_level_id) REFERENCES education_levels (id) ON DELETE SET NULL;
----------------------------------------------------------------------------------------
-
+-- Створення таблиці тегів
 CREATE TABLE tags (
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) UNIQUE NOT NULL
 );
 
--- Проміжна таблиця для зв'язку курсів і тегів
+-- Створення таблиці зв'язку курсів і тегів
 CREATE TABLE course_tags (
     course_id INT NOT NULL,
     tag_id INT NOT NULL,
     PRIMARY KEY (course_id, tag_id),
-    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES all_courses (id) ON DELETE CASCADE,
-    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tags (id) ON DELETE CASCADE
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES all_courses(id) ON DELETE CASCADE,
+    CONSTRAINT fk_tag FOREIGN KEY (tag_id) REFERENCES tags(id) ON DELETE CASCADE
 );
 
+-- Створення таблиці збережених курсів
 CREATE TABLE saved_courses (
-    id SERIAL PRIMARY KEY,                 
-    user_id INT NOT NULL,                 
-    course_id INT NOT NULL, 
-    CONSTRAINT fk_user
-        FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    CONSTRAINT fk_course
-        FOREIGN KEY (course_id) REFERENCES all_courses (id) ON DELETE CASCADE,
-    UNIQUE (user_id, course_id)           
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    course_id INT NOT NULL,
+    saved_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES all_courses(id) ON DELETE CASCADE,
+    UNIQUE (user_id, course_id)
 );
 
-
+-- Створення таблиці модулів
 CREATE TABLE modules (
     id SERIAL PRIMARY KEY,
     course_id INT NOT NULL,
-    title VARCHAR(100) NOT NULL, 
-    order_num INT NOT NULL,  -- порядок модуля в курсі
-    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES all_courses (id) ON DELETE CASCADE
+    title VARCHAR(100) NOT NULL,
+    order_num INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_course FOREIGN KEY (course_id) REFERENCES all_courses(id) ON DELETE CASCADE
 );
 
+-- Створення таблиці лекцій
 CREATE TABLE lectures (
     id SERIAL PRIMARY KEY,
     module_id INT NOT NULL,
     title VARCHAR(100) NOT NULL,
-    description TEXT,  -- текст лекції або посилання на відео
-    order_num INT NOT NULL,  -- порядок лекції в модулі
-    CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES modules (id) ON DELETE CASCADE
+    description TEXT,
+    order_num INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_module FOREIGN KEY (module_id) REFERENCES modules(id) ON DELETE CASCADE
 );
 
+-- Створення таблиці файлів лекцій
 CREATE TABLE lecture_files (
-    id SERIAL PRIMARY KEY,              
-    lecture_id INT NOT NULL,             
-    file_name VARCHAR(255) NOT NULL,     
-    file_url VARCHAR(1024) NOT NULL,     
-    file_type VARCHAR(50),             
-    CONSTRAINT fk_lecture FOREIGN KEY (lecture_id) REFERENCES lectures (id) ON DELETE CASCADE  
+    id SERIAL PRIMARY KEY,
+    lecture_id INT NOT NULL,
+    file_name VARCHAR(255) NOT NULL,
+    file_url VARCHAR(1024) NOT NULL,
+    file_type VARCHAR(50),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT fk_lecture FOREIGN KEY (lecture_id) REFERENCES lectures(id) ON DELETE CASCADE
 );
