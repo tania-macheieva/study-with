202c3e4d7761e0a4dfa918fdd892de0fdd8abcbf
AlinaPavlenk18 bc2024-6 <alinapavlenko01005@gmail.com>
2025-01-09 15:04:06 +0200

    The section for asking additional questions has been translated into UA.

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const authRoutes = require('./auth'); 
-require('dotenv').config();
+require('dotenv').config({path: './secret.env' });
 const passport = require('passport');
 const session = require('express-session');
 const stripeRoutes = require("./pay-page/stripe");
