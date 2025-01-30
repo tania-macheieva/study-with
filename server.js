bbc5e5eae07d82e39a64936af56e2efb7447bcb9
AlinaPavlenk18 bc2024-6 <alinapavlenko01005@gmail.com>
2025-01-30 21:40:37 +0200

    Added a page for passing the test itself and a backend to it.| Not the final version

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use("/auth", authRoutes);
+app.use('/tests', testRouter);
 
 
 const searchRouter = require('./searchRouter');
@@ -45,7 +46,7 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use('/tests', testRouter);
+
 
 app.use(express.static(path.join(__dirname)));
 
@@ -124,12 +125,18 @@ app.get('/test-creation', (req, res) => {
     res.sendFile(path.join(__dirname, './tests/test-creation.html')); 
 });
 
+app.get('/test-page', (req, res) => {
+    res.sendFile(path.join(__dirname, './tests/test-page.html')); 
+});
+
 app.use('/pay-page', stripeRoutes);
 app.use('/pay-page/webhook', express.raw({type: 'application/json'}));
 app.post('/pay-page/webhook', express.raw({type: 'application/json'}), (req, res) => {
     req.rawBody = req.body;
     next();
 });
+
+
 
 app.use("/pay-page", stripeRoutes);
 app.get('/get-stripe-key', (req, res) => {
@@ -148,6 +155,7 @@ app.use((err, req, res, next) => {
     console.error(err.stack);
     res.status(500).send('Something broke!');
 });
+
 
 const HOST = process.env.HOST || "localhost";
 const PORT = process.env.PORT || 8000;
