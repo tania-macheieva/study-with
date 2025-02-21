32742004885d9b07a1c0450c939b91f0bdd37fea
RomanMirka <romamirkastudy@gmail.com>
2025-02-21 16:33:46 +0200

    bug w no-registered user fixed

--- terms-cond/teacherview.js
+++ terms-cond/teacherview.js
@@ -1,16 +1,14 @@
 const userRole = localStorage.getItem('role');
 
-// Отримуємо всі елементи з відповідними класами
 const teacherContent = document.querySelectorAll('.teacher-content');
 const studentContent = document.querySelectorAll('.student-content');
 
 function showContentBasedOnRole() {
   if (userRole === 'teacher') {
     teacherContent.forEach(item => item.removeAttribute('hidden')); 
-  }
-  if (userRole === 'student') {
+  } else if (userRole === 'student') {
     studentContent.forEach(item => item.removeAttribute('hidden'));
-  }
+}
 }
 
 document.addEventListener('DOMContentLoaded', showContentBasedOnRole);
